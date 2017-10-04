0928e05e67777f773fe04476c5975bcc9bc320ac
Vincent PEILLEX <peillex.vincent@live.fr>
2017-10-04 19:20:12 +0200

    Merge des branches. Ajout d'actions avec les boutons de la page home.

--- app/src/main/java/info706/zikub/HomeActivity.java
+++ app/src/main/java/info706/zikub/HomeActivity.java
@@ -1,7 +1,12 @@
 package info706.zikub;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import info706.zikub.models.User;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -9,5 +14,30 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        final Button btnConnection = (Button)findViewById(R.id.btnConnexion);
+        final Button btnRegister = (Button)findViewById(R.id.btnInscription);
+
+        btnConnection.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        // Redirige directement l'utilisateur sur l'activité main si il possède déjà un token.
+        if(User.getOauthToken(getApplicationContext()) != null) {
+            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+            startActivity(intent);
+        }
     }
 }
--- app/src/main/java/info706/zikub/MainActivity.java
+++ app/src/main/java/info706/zikub/MainActivity.java
@@ -2,6 +2,9 @@ package info706.zikub;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import info706.zikub.models.User;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.d("OAUTH_TOKEN", User.getOauthToken(getApplicationContext()));
     }
 }
