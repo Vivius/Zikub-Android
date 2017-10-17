cedccb4da261e17b0b7f4210fbaa952d5332b427
Vincent PEILLEX <peillex.vincent@live.fr>
2017-10-17 21:49:09 +0200

    Amélioration du code du player

--- app/src/main/java/info706/zikub/MainActivity.java
+++ app/src/main/java/info706/zikub/MainActivity.java
@@ -7,7 +7,6 @@ import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.ListView;
@@ -15,6 +14,7 @@ import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
@@ -27,7 +27,6 @@ import info706.zikub.models.PlayList;
 import info706.zikub.models.Setting;
 import info706.zikub.models.User;
 import info706.zikub.services.PlaylistService;
-import info706.zikub.services.YoutubeService;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -37,10 +36,9 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class MainActivity extends AppCompatActivity {
 
     private YoutubePlayer youtubePlayer;
-    private int playlistId;
-    private List<Music> musics;
-
-    private int selectedMusic = -1;
+    private PlayList playList;
+    private Music selectedMusic = null;
+    private int selectedIndex = 0;
 
     public static final String PLAYLIST_ID_TAG = "PLAYLIST_ID";
     public static final String MUSIC_RANK_TAG = "MUSIC_RANK";
@@ -49,7 +47,11 @@ public class MainActivity extends AppCompatActivity {
     private ImageButton btnNext;
     private ImageButton btnPrevious;
     private ImageView cover;
-    private ListView playlist;
+    private ListView listView;
+
+    public MainActivity() {
+        playList = new PlayList();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -58,14 +60,14 @@ public class MainActivity extends AppCompatActivity {
 
         // Log.d("OAUTH_TOKEN", User.getOauthToken(getApplicationContext()));
 
-        playlist = (ListView) findViewById(R.id.playlist);
+        listView = (ListView) findViewById(R.id.playlist);
         youtubePlayer = new YoutubePlayer(this);
         btnPlayPause = (ImageButton)findViewById(R.id.btnPlayPause);
         btnNext = (ImageButton)findViewById(R.id.btnNext);
         btnPrevious = (ImageButton)findViewById(R.id.btnPrevious);
         cover = (ImageView) findViewById(R.id.cover);
 
-        // Chargement de la playlist de l'utilisateur.
+        // Chargement de la playList de l'utilisateur.
         Retrofit retrofit = new Retrofit.Builder()
             .baseUrl(Setting.API_HOST)
             .addConverterFactory(GsonConverterFactory.create())
@@ -77,18 +79,18 @@ public class MainActivity extends AppCompatActivity {
         caller.enqueue(new Callback<PlayList>() {
             @Override
             public void onResponse(Call<PlayList> call, Response<PlayList> response) {
-                playlistId = response.body().getId();
-                musics = response.body().getMusics();
+                playList = response.body();
 
-                Collections.sort(musics, new Comparator<Music>() {
+                // Tri de la playlist en fonction des rank.
+                Collections.sort(playList.getMusics(), new Comparator<Music>() {
                     @Override
                     public int compare(Music o1, Music o2) {
                         return  o1.getRank() > o2.getRank() ? 1 : -1;
                     }
                 });
 
-                ArrayAdapter adapter = new MusicAdapter(getApplicationContext(), R.layout.music_item, musics);
-                playlist.setAdapter(adapter);
+                ArrayAdapter adapter = new MusicAdapter(getApplicationContext(), R.layout.music_item, playList.getMusics());
+                listView.setAdapter(adapter);
             }
 
             @Override
@@ -97,21 +99,20 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        playlist.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 Intent intent = new Intent(getApplicationContext(), SearchActivity.class);
                 Bundle bundle = new Bundle();
                 bundle.putInt(MUSIC_RANK_TAG, position + 1);
-                bundle.putInt(PLAYLIST_ID_TAG, playlistId);
+                bundle.putInt(PLAYLIST_ID_TAG, playList.getId());
                 intent.putExtras(bundle);
                 startActivity(intent);
             }
         });
 
-        // TEST - Lancement de la lecture d'une musique Youtube dans l'application.
-        // youtubePlayer.start("https://www.youtube.com/watch?v=_cB3HXVvm0g");
-
+        // Evénement appelé lorque la musique est prête (que la lecture commence).
+        // Permet d'arrêter l'animation loader.
         youtubePlayer.setYoutubePlayerListener(new YoutubePlayerListener() {
             @Override
             public void onMusicBegin() {
@@ -121,14 +122,13 @@ public class MainActivity extends AppCompatActivity {
 
 
         // Bontons du lecteur
+
+        // Bouton Play / Pause
         btnPlayPause.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(selectedMusic == -1) {
-                    selectedMusic = 0;
-                    youtubePlayer.start(musics.get(selectedMusic).getUrl());
-                    Picasso.with(getApplicationContext()).load(musics.get(selectedMusic).getCover()).resize(200,200).centerCrop().into(cover);
-                }
+                if(selectedMusic == null)
+                    startMusicByIndex(0);
 
                 if(youtubePlayer.isPlaying()) {
                     btnPlayPause.setImageResource(R.drawable.ic_btnplay);
@@ -140,29 +140,19 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        // start musique précédente
+        // Bouton musique suivante
         btnPrevious.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(selectedMusic > 0) {
-                    selectedMusic--;
-                    youtubePlayer.start(musics.get(selectedMusic).getUrl());
-                    Picasso.with(getApplicationContext()).load(musics.get(selectedMusic).getCover()).resize(200,200).centerCrop().into(cover);
-                    btnPlayPause.setImageResource(R.drawable.ic_btnpause);
-                }
+                startMusicByIndex(selectedIndex - 1);
             }
         });
 
-        // start musique suivante
+        // Bouton musique précédente
         btnNext.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(selectedMusic < 5) {
-                    selectedMusic++;
-                    youtubePlayer.start(musics.get(selectedMusic).getUrl());
-                    Picasso.with(getApplicationContext()).load(musics.get(selectedMusic).getCover()).resize(200,200).centerCrop().into(cover);
-                    btnPlayPause.setImageResource(R.drawable.ic_btnpause);
-                }
+                startMusicByIndex(selectedIndex + 1);
             }
         });
 
@@ -174,5 +164,22 @@ public class MainActivity extends AppCompatActivity {
         // Stop le lecteur si on change d'activité.
         youtubePlayer.pause();
         btnPlayPause.setImageResource(R.drawable.ic_btnplay);
+    }
+
+    /**
+     * Permet de changer de musique en spécifiant son index (rank).
+     * Met à jour l'ensemble de l'interface.
+     * Ne fait rien si l'index spécifié n'existe pas.
+     *
+     * @param index int
+     */
+    private void startMusicByIndex(int index) {
+        if(index >= 0 && index <= playList.getMusics().size()) {
+            selectedIndex = index;
+            selectedMusic = playList.getMusics().get(index);
+            youtubePlayer.start(selectedMusic.getUrl());
+            Picasso.with(getApplicationContext()).load(selectedMusic.getCover()).resize(200,200).centerCrop().into(cover);
+            btnPlayPause.setImageResource(R.drawable.ic_btnpause);
+        }
     }
 }
