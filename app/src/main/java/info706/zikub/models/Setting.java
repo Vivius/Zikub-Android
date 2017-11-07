ed915fd847e25af924cf92a175b2ebd4bec5a47d
Vincent PEILLEX <peillex.vincent@live.fr>
2017-11-07 18:28:09 +0100

    Modification de l'host et passage en https. Blocage du mode paysage de l'application.

--- app/src/main/java/info706/zikub/models/Setting.java
+++ app/src/main/java/info706/zikub/models/Setting.java
@@ -1,6 +1,6 @@
 package info706.zikub.models;
 
 public class Setting {
-    public final static String API_HOST = "http://vincent-peillex.fr/api/";
+    public final static String API_HOST = "https://zikub.vincent-peillex.fr/api/";
     // public final static String API_HOST = "http://192.168.10.10/api/";
 }
